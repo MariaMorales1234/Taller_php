5849f1b79c6e67b1e495e82e9bb22fed4bdbad2b
María Morales1234 <mjmorales@uniboyaca.edu.co>
2025-09-20 13:00:04 -0500

    procedimientos para los resultados

--- procesar.php
+++ procesar.php
@@ -14,3 +14,46 @@ if ($_SERVER["$REQUEST_METHOD"] == "$_POST"){
     sort($diferenciaBA);
 }
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultados</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Resultados</h2>
+        <div class="resultados">
+            <p>
+                <b>Conjunto A:</b>
+                {<?= implode(",", $A)?>}
+            </p>
+            <p>
+                <b>Conjunto B:</b>
+                {<?= implode(",", $B)?>}
+            </p>
+            <hr>
+            <p>
+                <b>Union: </b>
+                {<?= implode(",", $union)?>}
+            </p>
+            <p>
+                <b>interseccion: </b>
+                {<?= implode(",", $interseccion)?>}
+            </p>
+            <p>
+                <b>diferencia AB: </b>
+                {<?= implode(",", $diferenciaAB)?>}
+            </p>
+            <p>
+                <b>diferencia BA: </b>
+                {<?= implode(",", $diferenciaBA)?>}
+            </p>
+        </div>
+        <br>
+        <a href="index.html">Volver</a>
+    </div>
+</body>
+</html>
