be0535ee885d89fee237cece7e28b8bd6b447e07
anamaria046 <ammorar@uniboyaca.edu.co>
2025-09-24 16:12:42 -0500

    Se agrego el form

--- index.php
+++ index.php
@@ -8,8 +8,25 @@
 </head>
 <body>
     <h1>¿Mi Número es divisible?</h1>
+    <h2>Ten en cuenta que los númerosdeben de ser enteros positivos</h2>
     <div class="container">
-        <form action="index.php" method="post"></form>
+        <form action="index.php" method="post">
+            <label for="num1">Número 1:</label>
+        <input type="text" id="num1" name="num1"
+            value="
+        <?php 
+        echo isset($_POST['num1']) ? htmlspecialchars($_POST['num1']) : ''; 
+        ?>">
+
+        <label for="num2">Número 2:</label>
+        <input type="text" id="num2" name="num2"
+               value="
+        <?php 
+        echo isset($_POST['num2']) ? htmlspecialchars($_POST['num2']) : ''; 
+        ?>">
+
+        <button type="submit" name="comprobar">Comprobar</button>
+        </form>
     </div>
     
 </body>
