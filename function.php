113469d966e86d0a93c5e45efb77217cfe0324e6
oscar-LCP <ofcruz@uniboyaca.edu.co>
2025-09-24 16:35:51 -0500

    Corrección ingresos de datos

--- function.php
+++ function.php
@@ -1,21 +1,32 @@
 <?php
-    if(isset($_POST['numeros'])){
-    $numeros = array_map('floatval', explode(',', $_POST['numeros']));
+if(isset($_POST['numeros'])){
+    $numeros = array_map('trim', explode(',', $_POST['numeros']));
+    
+    foreach($numeros as $n){
+        if(!is_numeric($n)){
+            echo "Error: solo se permiten números separados por comas.";
+            exit;
+        }
+    }
+
+    $numeros = array_map('floatval', $numeros);
 
     if(count($numeros) > 0){
         $promedio = array_sum($numeros) / count($numeros);
-    
-    sort($numeros);
+ 
+        sort($numeros);
         $count = count($numeros);
         if($count % 2 == 0){
             $media = ($numeros[$count/2 - 1] + $numeros[$count/2]) / 2;
         } else {
             $media = $numeros[floor($count/2)];
-    }
+        }
 
-    $valores = array_count_values($numeros);
+        $valores = array_count_values(array_map('strval', $numeros));
         $maximo = max($valores);
         $moda = array_keys($valores, $maximo);
+
+        $moda = array_map('floatval', $moda);
     }
 } else {
     echo "No se enviaron números.";
